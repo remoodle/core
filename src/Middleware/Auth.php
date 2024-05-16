ddee16e1ead7c961d2dbcb821ac1ab494461ff2d
Nariman <sailaubek.nar@gmail.com>
2024-05-16 17:03:26 +0500

    feat: add rate limit

--- src/Middleware/Auth.php
+++ src/Middleware/Auth.php
@@ -9,6 +9,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Spiral\RoadRunner\KeyValue\Serializer\IgbinarySerializer;
 use Spiral\RoadRunner\KeyValue\StorageInterface;
 
 final class Auth implements MiddlewareInterface
@@ -17,6 +18,7 @@ final class Auth implements MiddlewareInterface
 
     public function __construct(
         private StorageInterface $storage,
+        private \Spiral\RoadRunner\KeyValue\Factory $factory
     ) {
     }
 
@@ -26,11 +28,19 @@ final class Auth implements MiddlewareInterface
             throw new \Exception(static::TOKEN_HEADER . ' header is required.', StatusCodeInterface::STATUS_UNPROCESSABLE_ENTITY);
         }
 
-        [$headerTokenValidStatus, $validatedRequest] = $this->headerTokenIsValid($request);
+        [$headerTokenValidStatus, $validatedRequest, $token] = $this->headerTokenIsValid($request);
 
         if(!$headerTokenValidStatus) {
             throw new \Exception("Request is not authorized.", StatusCodeInterface::STATUS_UNAUTHORIZED);
         }
+
+        $rateLimitStorage = $this->factory->select('rate-limit-token');
+
+        $currentRate = (int) $rateLimitStorage->get($token);
+        if($currentRate > 1000) {
+            throw new \Exception("Too many requests", StatusCodeInterface::STATUS_TOO_MANY_REQUESTS);
+        }
+        $rateLimitStorage->set($token, $currentRate + 1);
 
         return $handler->handle($validatedRequest);
     }
@@ -46,9 +56,9 @@ final class Auth implements MiddlewareInterface
         $user = $this->storage->get($token);
 
         if(!$user) {
-            return [false, $request];
+            return [false, $request, $token];
         }
 
-        return [true, $request->withAttribute("user", $user)];
+        return [true, $request->withAttribute("user", $user, null)];
     }
 }
